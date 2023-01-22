d1853a0b33d43e223e679880b610faefa12c9f34
Ehtisham Sabir <Shaamdbz@gmail.com>
2023-01-23 02:16:32 +0500

    week 6 q4

--- main.py
+++ main.py
@@ -5,32 +5,33 @@ import logging
 logging.basicConfig(filename="logger.log", format='%(message)s',
                     level=logging.INFO, filemode='w')
 
+
 def mapper():
+    # Also make sure to fill out the reducer code before clicking "Test Run" or "Submit".
 
-    #Also make sure to fill out the reducer code before clicking "Test Run" or "Submit".
+    # Each line will be a comma-separated list of values. The
+    # header row WILL be included. Tokenize each row using the
+    # commas, and emit (i.e. print) a key-value pair containing the
+    # district (not state) and Aadhaar generated, separated by a tab.
+    # Skip rows without the correct number of tokens and also skip
+    # the header row.
 
-    #Each line will be a comma-separated list of values. The
-    #header row WILL be included. Tokenize each row using the
-    #commas, and emit (i.e. print) a key-value pair containing the
-    #district (not state) and Aadhaar generated, separated by a tab.
-    #Skip rows without the correct number of tokens and also skip
-    #the header row.
+    # You can see a copy of the the input Aadhaar data
+    # in the link below:
+    # https://www.dropbox.com/s/vn8t4uulbsfmalo/aadhaar_data.csv
 
-    #You can see a copy of the the input Aadhaar data
-    #in the link below:
-    #https://www.dropbox.com/s/vn8t4uulbsfmalo/aadhaar_data.csv
-
-    #Since you are printing the output of your program, printing a debug
-    #statement will interfere with the operation of the grader. Instead,
-    #use the logging module, which we've configured to log to a file printed
-    #when you click "Test Run". For example:
-    #logging.info("My debugging message")
+    # Since you are printing the output of your program, printing a debug
+    # statement will interfere with the operation of the grader. Instead,
+    # use the logging module, which we've configured to log to a file printed
+    # when you click "Test Run". For example:
+    # logging.info("My debugging message")
     #
-    #Note that, unlike print, logging.info will take only a single argument.
-    #So logging.info("my message") will work, but logging.info("my","message") will not.
+    # Note that, unlike print, logging.info will take only a single argument.
+    # So logging.info("my message") will work, but logging.info("my","message") will not.
+
     header = True
     colnum = 12
-    col_district = 3    # col 4 District
+    col_district = 3  # col 4 District
     col_addhaargen = 8  # col 9 Aadhaar generated
     for line in sys.stdin:
         if header:
@@ -45,7 +46,5 @@ def mapper():
         result = '{0}\t{1}'.format(data[col_district], data[col_addhaargen])
         print(result)
 
+
 mapper()
-
-
-
